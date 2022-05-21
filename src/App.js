4642e8b0f7dfa91e090e1b14f17b728335e88ad3
Shahzod2727 <shahzodjava@gmail.com>
2022-05-21 16:38:36 +0500

    react router v6

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
 import React from "react";
-import './categories.styles.scss';
-import Directory from './components/directory/directory.component';
-const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-    },
-  ];
+import "./categories.styles.scss";
+import Navigation from "./routes/navigation/navigation.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
 
+const Shop = () => {
+  return <div> <h1>Shop page</h1></div>;
+};
+
+
+
+const App = () => {
   return (
-    <Directory categories={categories} />
+    // <Directory categories={categories} />
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />}></Route>
+        <Route path="/shop" element={<Shop />}></Route>
+      </Route>
+    </Routes>
   );
 };
 
